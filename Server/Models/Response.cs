45e4adb22873b8343e5b404fa4abf2f454a0b988
extern_TMa <T.Maracineanu@insiders-technologies.de>
2022-04-02 16:24:48 +0300

    Sync

---
+++ Server/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace GeoQuestions.Server.Models
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,4 @@
+using GeoQuestions.Server.Models;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<GeoQuestionContext>();
 
 var app = builder.Build();
 
